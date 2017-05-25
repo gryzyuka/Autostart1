5fe1949f5c4549364c7860ea8c273c485990bd91
Your Namegryzyuka <gryzyuka@gmail.com>
2017-05-25 21:34:24 +0300

    adding  project StartAutomation1

---
+++ src/main/StartAutomation1.java
@@ -0,0 +1,16 @@
+package main;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created by gryzyuka on 21.05.2017.
+ */
+public class StartAutomation1 {
+    public static void main(String[] args) {
+        WebDriver webDriver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        webDriver.get("http://www.yakaboo.ua");
+        System.out.println(webDriver.getTitle());
+    }
+}
