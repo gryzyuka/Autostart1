bcba0c495f5d1043c6a3fd9a3dc77d846b615ad3
Your Namegryzyuka <gryzyuka@gmail.com>
2017-05-27 11:56:07 +0300

    woring chrome opening

--- src/test/java/StartAutomation1.java
+++ src/test/java/StartAutomation1.java
@@ -1,14 +1,35 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 /**
  * Created by gryzyuka on 21.05.2017.
  */
 public class StartAutomation1 {
-    public static void main(String[] args) {
-        WebDriver webDriver = new ChromeDriver();
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        webDriver.get("http://www.yakaboo.ua");
-        System.out.println(webDriver.getTitle());
+    WebDriver driver;
+
+    @BeforeTest
+    public void before() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\gryzyuka\\.m2\\repository\\webdriver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
+
+    @Test
+    public void firstTest() throws InterruptedException {
+        driver.get("http://magento.brainacad.com/");
+        driver.findElement(By.cssSelector("#search")).sendKeys("PC games");
+        Thread.sleep(5000);
+    }
+
+    @AfterTest
+    public void after() {
+        driver.close();
+    }
+
 }
+
